f657561aad616d4dc5caec5bfeb1c7254dced050
davina777 <desu.avinash777@gmail.com>
2023-06-18 00:13:27 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Az_Demos_Repo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
